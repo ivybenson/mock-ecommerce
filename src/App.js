350cb316b38063eadf87ff120ff01e9a1d9bcdd7
Ivy Benson <irosebenson@gmail.com>
2021-03-23 08:05:20 -0500

    added some html to the front

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">store front</div>;
+  return (
+    <div className="App">
+      <header>Store Front</header>
+      <section>ideas for a bunch of products</section>{" "}
+      <footer>store front copyright</footer>
+    </div>
+  );
 }
 
 export default App;
