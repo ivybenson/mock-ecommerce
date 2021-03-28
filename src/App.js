77f759abb6eb1957b1fcf56ff14213b7ef668738
Ivy Benson <irosebenson@gmail.com>
2021-03-28 12:59:02 -0500

    added images

--- src/App.js
+++ src/App.js
@@ -3,9 +3,31 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header>Store Front</header>
-      <section>ideas for a bunch of products</section>{" "}
-      <footer>store front copyright</footer>
+      <header>Store Front home products about shop</header>
+      <section>
+        {" "}
+        this is going to be a separate component - ideas for a bunch of products
+      </section>{" "}
+      <footer>
+        <p>© Ivy Benson</p>
+        <a
+          name="contacts"
+          href="https://www.linkedin.com/in/ivy-benson-407b27144/"
+        >
+          <img
+            src="images/linkedin.png"
+            width="30"
+            height="30"
+            alt="linkedin"
+          />
+        </a>
+        <a href="https://github.com/ivybenson">
+          <img src="images/github.png" width="30" height="30" alt="github" />
+        </a>
+        <a href="mailto:irosebenson@email.com">
+          <img src="images/gmail.jpg" width="30" height="30" alt="email" />
+        </a>
+      </footer>
     </div>
   );
 }
